79ecb16580aa19569e41d6e6023e2f34970089a8
neverbot <ivan@neverbot.com>
2026-03-24 23:30:05 +0100

    style: add JSDoc and move export to end of event-emitter

--- src/utils/event-emitter.js
+++ src/utils/event-emitter.js
@@ -1,8 +1,14 @@
-export default class EventEmitter {
+/** Simple pub/sub event emitter using a Map of event name to callback arrays. */
+class EventEmitter {
   constructor() {
     this.events = new Map();
   }
 
+  /**
+   * Register a callback for an event.
+   * @param {string} event
+   * @param {Function} callback
+   */
   on(event, callback) {
     if (!this.events.has(event)) {
       this.events.set(event, []);
@@ -10,12 +16,22 @@ export default class EventEmitter {
     this.events.get(event).push(callback);
   }
 
+  /**
+   * Emit an event, calling all registered callbacks.
+   * @param {string} event
+   * @param {*} data
+   */
   emit(event, data) {
     if (this.events.has(event)) {
       this.events.get(event).forEach((callback) => callback(data));
     }
   }
 
+  /**
+   * Remove a specific callback from an event.
+   * @param {string} event
+   * @param {Function} callback
+   */
   off(event, callback) {
     if (this.events.has(event)) {
       const listeners = this.events.get(event).filter((cb) => cb !== callback);
@@ -23,7 +39,10 @@ export default class EventEmitter {
     }
   }
 
+  /** Remove all listeners for all events. */
   removeAllListeners() {
     this.events.clear();
   }
 }
+
+export default EventEmitter;
